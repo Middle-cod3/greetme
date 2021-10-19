7cd0b1d787bd4dd3f51e1d0c3557c2b1933a91e9
Ayan <ayanr143@gmail.com>
2021-10-19 12:09:38 +0530

    v1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+let date = new Date();
+let hour = date.getHours();
+
+let greeting = "";
+// Its a inline CSS
+let cssStyle={};
+
+if (hour >= 5 && hour < 12) {
+  greeting = "Good Moring..!";
+  cssStyle.color="Green";
+} else if (hour >= 12 && hour < 14) {
+  greeting = "Good Afternoon..!";
+  cssStyle.color="#e0c73a";
+
+} else if (hour >= 14 && hour < 18) {
+  greeting = "Good Noon..!";
+  cssStyle.color="#f2b527";
+
+} else if (hour >= 18 && hour < 20) {
+  greeting = "Good Evening..!";
+  cssStyle.color="#adb4f0";
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <div>
+  <h1 className="heading">
+    Hi Ayan, <span style={cssStyle}>{greeting}</span>
+  </h1>
+  
+  </div>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+);
