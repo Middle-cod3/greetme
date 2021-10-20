dfee43d9124e4163f9d9a321fd41e82559a977ec
Ayan <ayanr143@gmail.com>
2021-10-20 19:07:02 +0530

    blur

--- src/index.js
+++ src/index.js
@@ -7,39 +7,34 @@ let hour = date.getHours();
 
 let greeting = "";
 // Its a inline CSS
-let cssStyle={};
+let cssStyle = {};
 
 if (hour >= 5 && hour < 12) {
   greeting = "Good Moring..!";
-  cssStyle.color="Green";
+  cssStyle.color = "Green";
 } else if (hour >= 12 && hour < 14) {
   greeting = "Good Noon..!";
-  cssStyle.color="#e0c73a";
-
+  cssStyle.color = "#e0c73a";
 } else if (hour >= 14 && hour < 18) {
   greeting = "Good Afternoon..!";
-  cssStyle.color="#f2b527";
-
+  cssStyle.color = "#f2b527";
 } else if (hour >= 18 && hour < 20) {
   greeting = "Good Evening..!";
-  cssStyle.color="#adb4f0";
-
-}
- else if (hour >= 20 && hour < 23) {
+  cssStyle.color = "#adb4f0";
+} else if (hour >= 20 && hour < 23) {
   greeting = "Good Night..!";
-  cssStyle.color="#394557";
-
-};
-let tm=new Date().toLocaleTimeString();
+  cssStyle.color = "#394557";
+}
+let tm = new Date().toLocaleTimeString();
 
 
 ReactDOM.render(
   <div>
-  
-  <h1 className="heading">
-    Hi Buddy😍, <span style={cssStyle}>{greeting}🌼</span><span className="tms">{tm}</span>
-  </h1>
-  
+    <h1 className="heading">
+      Hi Buddy😍, <span style={cssStyle}>{greeting}🌼</span>
+      <span className="tms">{tm}</span>
+    </h1>
+    
   </div>,
 
   document.getElementById("root")
